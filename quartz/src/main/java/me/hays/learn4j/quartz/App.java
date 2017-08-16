c84ecc00aaa3a51428a61cb618b24c3fa9e0c38e
Hays4Git <Hays4Git@163.com>
2017-08-16 16:02:35 +0800

    转为maven，模块quartz

---
+++ quartz/src/main/java/me/hays/learn4j/quartz/App.java
@@ -0,0 +1,13 @@
+package me.hays.learn4j.quartz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
