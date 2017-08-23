6c5299b7ebede4a63712200babfbf7a4ef2ca7e6
Hays4Git <Hays4Git@163.com>
2017-08-23 11:55:17 +0800

    增加模块

---
+++ cache/src/main/java/me/hays/learn4j/cache/App.java
@@ -0,0 +1,13 @@
+package me.hays.learn4j.cache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
